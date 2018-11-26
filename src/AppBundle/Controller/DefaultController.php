fc269e6256b6a2afc75c8c7a3f97a4a4b2f06ee6
Nour Ben Arfa <benarfanour9@gmail.com>
2018-11-26 01:15:22 +0100

    test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,42 +7,74 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Mail ;
 use AppBundle\Form\MailType ;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest; // alias pour toutes les annotations
+
 
 use Symfony\Component\HttpFoundation\Response;
 
 
-class DefaultController extends Controller
+
+
+class DefaultController extends FOSRestController
 {
-   
+
+    
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     * @Rest\Post("/send")
+     */
+
     public function indexAction(Request $request)
     {
-    $mail = new Mail();
-    $form= $this->createForm(MailType::class, $mail);
-    $form->handleRequest($request) ;
-    if ($form->isValid()) {
-    $message = \Swift_Message::newInstance()
-                ->setSubject('Accusé de réception')
-                ->setFrom('nour.benarfa@esprit.tn')
-                ->setTo($mail->getEmail())
-                ->setBody(
-                $this->renderView(
-                'mail.html.twig',
-            array('nom' => $mail->getNom(), 'prenom'=>$mail->getPrenom())
-                ),
-            'text/html'
-                );
+   // $mail = new Mail();
+    //$form= $this->createForm(MailType::class, $mail);
+    //$form->handleRequest($request) ;
+    //if ($form->isValid()) {
 
-        
-            $this->get('mailer')->send($message);
+    	//$securityContext = $this->container->get('security.authorization_checker');
+			//if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
+    
+
+					    $message = \Swift_Message::newInstance()
+					                ->setSubject('Accusé de réception')
+					                ->setFrom('nour.benarfa@esprit.tn')
+					                ->setTo()
+					                 		
+					                  
+					                ->setBody(
+					                $this->renderView('mail.html.twig'),
+					            'text/html'
+					                );
+
+					        
+					            $this->get('mailer')->send($message);
+
+					        $response = new Response(json_encode(array('success' => true)));
+					        $response->headers->set('Content-Type', 'application/json');
+
+					        return $response;
+					    }
+
+			//}
+
+			
 
 
-            if (! $this->get('mailer')->send($message)) return Response ('failed to send') ;
-            return $this->redirect($this->generateUrl('my_app_mail_accuse'));
+				//return $this->redirectToRoute('fos_user_security_login');
+			
+
+				
+			
+
+
+            //if (! $this->get('mailer')->send($message)) return Response ('failed to send') ;
+            //return $this->redirect($this->generateUrl('my_app_mail_accuse'));
 
 }
-        return $this->render('mail/index.html.twig', array('form'=>$form
-        ->createView()));
-    }
+       // return $this->render('mail/index.html.twig', array('form'=>$form
+        //->createView()));
+  //    }
 
 
 
